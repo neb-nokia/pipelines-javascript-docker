45cbeba1cc0e769a219836e1ed04afec57fbcf63
neb-nokia <77461358+neb-nokia@users.noreply.github.com>
2021-10-11 16:50:32 -0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 
-const PORT = 8080;
+const PORT = 8088;
 const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world on port 8088\n');
 });
 
 app.listen(PORT, HOST);
